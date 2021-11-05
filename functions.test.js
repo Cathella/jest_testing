bb4685e8168ac710057e2676cd7133dfed3cc8b2
Cathella <ncathie214@gmail.com>
2021-11-05 15:31:33 +0300

    testing async functions

--- functions.test.js
+++ functions.test.js
@@ -42,3 +42,11 @@ test('Admin should be in usernames', () => {
   usernames = ['catherine', 'benny', 'admin'];
   expect(usernames).toContain('admin');
 });
+
+// working with async data
+test('User fetched name should be Leanne Graham', () => {
+  // expect.assertions(1);
+  return functions.fetchUser().then(data => {
+      expect(data.name).toEqual('Leanne Graham');
+    });
+});
